f696ac6b9713c78ba41c4f92b6143b9ceacbc385
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-14 13:49:06 +0900

    feat(InputView) : 추가 사용자의 이동메시지 게임 시도 여부 메시지 출력, 이에 따른 사용자의 입력 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -4,9 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public String readBridgeSize() {
         System.out.println("다리 건너기 게임을 시작합니다."+"\n");
         System.out.println("다리의 길이를 입력해주세요.");
@@ -14,17 +11,13 @@ public class InputView {
         return bridgeSize;
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주새요. (위: U, 아래: D)");
+        return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        return Console.readLine();
     }
 }
