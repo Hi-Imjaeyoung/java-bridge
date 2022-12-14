ed3e60cdfafad6523b0642c0f6a4538991d3f98f
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-14 14:22:59 +0900

    feat(Bridge) : 추가 size의 validate 기능

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,22 +1,31 @@
 package bridge;
 
+import javax.naming.InsufficientResourcesException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Bridge {
     private int size;
-    private List<Integer> bridgeInformation;
+    private List<String> bridgeInformation;
     public Bridge(String size){
-        validateSize(size);
-        this.size = Integer.parseInt(size);
+        this.size = validateSize(size);
     }
     public void makeBridge(){
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeInformation = bridgeMaker.makeBridge(this.size);
     }
-    public void makeCustomBridge (){
-
+    public void makeCustomBridge (List<String> bridgeInformation){
+        this.bridgeInformation = bridgeInformation;
     }
-    private void validateSize(String size) throws IllegalArgumentException{
-
+    private int validateSize(String size) throws IllegalArgumentException{
+        if(size.equals(null) || size.replaceAll("[^0-9]","").length() != size.length()){
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+        int sizeNumericValue = Integer.parseInt(size);
+        if(sizeNumericValue<3 || sizeNumericValue>20){
+            throw new IllegalArgumentException("3이상 20이하의 숫자를 입력해주세요");
+        }
+        return sizeNumericValue;
     }
 
 }
